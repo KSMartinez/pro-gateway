eeafca97087f4553a3ea9f5bf1187f43b1608aa4
Akhil Muraleedharan <akhil.muraleedharan@actimage.com>
2022-03-23 16:29:38 +0100

    Created factories and fixtures for Offers using Foundry

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,9 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\DomainFactory;
+use App\Factory\OfferFactory;
+use App\Factory\TypeOfContractFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +12,9 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        DomainFactory::createMany(5);
+        TypeOfContractFactory::createMany(8);
+        OfferFactory::createMany(20);
 
         $manager->flush();
     }
---
+++ src/Factory/OfferFactory.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Offer;
+use App\Repository\OfferRepository;
+use Zenstruck\Foundry\RepositoryProxy;
+use Zenstruck\Foundry\ModelFactory;
+use Zenstruck\Foundry\Proxy;
+
+/**
+ * @extends ModelFactory<Offer>
+ *
+ * @method static Offer|Proxy createOne(array $attributes = [])
+ * @method static Offer[]|Proxy[] createMany(int $number, array|callable $attributes = [])
+ * @method static Offer|Proxy find(object|array|mixed $criteria)
+ * @method static Offer|Proxy findOrCreate(array $attributes)
+ * @method static Offer|Proxy first(string $sortedField = 'id')
+ * @method static Offer|Proxy last(string $sortedField = 'id')
+ * @method static Offer|Proxy random(array $attributes = [])
+ * @method static Offer|Proxy randomOrCreate(array $attributes = [])
+ * @method static Offer[]|Proxy[] all()
+ * @method static Offer[]|Proxy[] findBy(array $attributes)
+ * @method static Offer[]|Proxy[] randomSet(int $number, array $attributes = [])
+ * @method static Offer[]|Proxy[] randomRange(int $min, int $max, array $attributes = [])
+ * @method static OfferRepository|RepositoryProxy repository()
+ * @method Offer|Proxy create(array|callable $attributes = [])
+ */
+final class OfferFactory extends ModelFactory
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        // TODO inject services if required (https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services)
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    protected function getDefaults(): array
+    {
+        return [
+            // TODO add your default values here (https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories)
+            'title' => self::faker()->text(),
+            'description' => self::faker()->text(),
+            'city' => self::faker()->text(),
+            'country' => self::faker()->text(),
+            'datePosted' => self::faker()->dateTime(),
+            'publishDuration' => self::faker()->randomNumber(),
+            'domain' => DomainFactory::random(),
+            'typeOfContract' => TypeOfContractFactory::random(),
+            'minSalary' => self::faker()->randomNumber(),
+            'maxSalary' => self::faker()->randomNumber(),
+            'companyName' => self::faker()->text(),
+            'isDirect' => self::faker()->boolean(),
+            'isValid' => true,
+            'isPublic' => self::faker()->boolean(),
+            'isOfPartner' => self::faker()->boolean(),
+        ];
+    }
+
+    protected function initialize(): self
+    {
+        // see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+        return $this
+            // ->afterInstantiate(function(Offer $offer): void {})
+        ;
+    }
+
+    protected static function getClass(): string
+    {
+        return Offer::class;
+    }
+}
