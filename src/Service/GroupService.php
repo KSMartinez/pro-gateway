492be774fe3c40b7ffa6290c557f872989424aa7
Akhil Muraleedharan <akhil.muraleedharan@actimage.com>
2022-04-26 15:06:24 +0200

    Added NotifcationService and EmailNotificationService and refactored OfferAlertService

--- src/Service/GroupService.php
+++ src/Service/GroupService.php
@@ -3,13 +3,14 @@
 namespace App\Service;
 use App\Entity\Group;
 use App\Entity\GroupStatus;
+use App\Entity\NotificationSource;
+use App\Model\GroupDemand;
 use App\Repository\GroupRepository;
 use App\Repository\GroupStatusRepository;
 
 class GroupService
 {
-
-    public function __construct(private GroupRepository $groupRepository, private GroupStatusRepository $groupStatusRepository)
+    public function __construct(private GroupRepository $groupRepository, private GroupStatusRepository $groupStatusRepository, private NotificationService $notificationService)
     {
     }
 
@@ -20,5 +21,24 @@ class GroupService
     {
         $groupDemandStatus = $this->groupStatusRepository->findOneBy(array('status' => GroupStatus::EN_ATTENTE));
         return $this->groupRepository->findBy(array('groupStatus' => $groupDemandStatus));
+    }
+
+    /**
+     * @param GroupDemand $groupDemand
+     * @return Group
+     */
+    public function validateGroupDemand(GroupDemand $groupDemand): Group
+    {
+        $groupConfirmedStatus = $this->groupStatusRepository->findOneBy(array('status' => GroupStatus::CONFIRME));
+        $groupDemand->getGroup()->setGroupStatus($groupConfirmedStatus);
+        $this->notificationService->createNotification($groupDemand->getNotificationMessage(), NotificationSource::GROUP_DEMAND);
+        return $groupDemand;
+    }
+
+    public function rejectGroupDemand(Group $groupDemand): Group
+    {
+        $groupRefusedStatus = $this->groupStatusRepository->findOneBy(array('status' => GroupStatus::REFUSE));
+        $groupDemand->setGroupStatus($groupRefusedStatus);
+        return $groupDemand;
     }
 }
---
+++ src/Service/NotificationService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Notification;
+use App\Entity\User;
+use App\Repository\NotificationSourceRepository;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+use Exception;
+
+class NotificationService
+{
+    public function __construct(private EntityManagerInterface $entityManager, private NotificationSourceRepository $notificationSourceRepository)
+    {
+    }
+
+    /**
+     * A new notification is created for each notification.
+     * //todo review squashing notifications together especially for Offers. Make new function that creates unique notifications?
+     * @throws Exception
+     */
+    public function createNotification(string $notificationMessage, string $source, User $user)
+    {
+        $groupDemandNotificationSource = $this->notificationSourceRepository->findOneBy(array('sourceLabel' => $source));
+        if (!$groupDemandNotificationSource) {
+            throw new Exception("Notification Source for " . $source . " not found. Please add this to the notificationSource table");
+        }
+        $notification = new Notification();
+        $notification->setSource($groupDemandNotificationSource)
+                     ->setUser($user)
+                     ->setMessage($notificationMessage)
+                     ->setCreatedOn(new DateTime('now'));
+        $this->entityManager->persist($notification);
+        $this->entityManager->flush();
+    }
+}
