cc4a5e96d2a52703f19efb0d83a28d552a7aad3b
Louise Caty <louise.caty@actimage.com>
2022-05-23 15:02:19 +0200

    sprint 2 : adding fields in User

---
+++ migrations/Version20220523125710.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220523125710 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD mentor_accept TINYINT(1) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user DROP mentor_accept');
+    }
+
+        public function isTransactional(): bool
+    {
+        return false;
+    }
+}
