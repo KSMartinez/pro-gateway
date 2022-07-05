3f4c105bdf876d58c0bd6557c4cccc3cbcd4606b
Akhil Muraleedharan <akhil.muraleedharan@actimage.com>
2022-07-05 17:10:28 +0200

    Removed Foundry from the Subscriber and user a normal repo instead.
    Lesson here: Do not use dev libraries for non-dev code even if said code is used only in non-dev.

--- src/EventSubscriber/FakeUserLoginSubscriber.php
+++ src/EventSubscriber/FakeUserLoginSubscriber.php
@@ -3,13 +3,13 @@
 namespace App\EventSubscriber;
 
 use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\HttpKernel\Event\ControllerEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
-use function Zenstruck\Foundry\factory;
 
 /**
  * This event subscriber logs all events with a fake user with user role.
@@ -20,8 +20,9 @@ class FakeUserLoginSubscriber implements EventSubscriberInterface
     /**
      * @param TokenStorageInterface $tokenStorage
      * @param SessionInterface      $session
+     * @param UserRepository        $userRepository
      */
-    public function __construct(private TokenStorageInterface $tokenStorage, private SessionInterface $session)
+    public function __construct(private TokenStorageInterface $tokenStorage, private SessionInterface $session, private UserRepository $userRepository)
     {
     }
 
@@ -42,9 +43,8 @@ class FakeUserLoginSubscriber implements EventSubscriberInterface
     public function onKernelController(ControllerEvent $event)
     {
         /** @var User $user */
-        $user = factory(User::class)
-            ->find(['email' => 'fakeuser@email.com'])
-            ->object();
+        $user = $this->userRepository
+            ->find(['email' => 'fakeuser@email.com']);
 
         //Handle getting or creating the user entity likely with a posted form
         // The third parameter "main" can change according to the name of your firewall in security.yml
