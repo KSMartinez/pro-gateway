0bcfdad3d0f396e6a22efaa1abb25eace276850c
Ezequiel Munoz <ezequiel.munoz@actimage.com>
2022-07-13 10:57:18 +0200

    Added DataProvider for Image stock

---
+++ src/DataProvider/ImageStockDataProvider.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\DataProvider;
+
+use ApiPlatform\Core\DataProvider\CollectionDataProviderInterface;
+use ApiPlatform\Core\DataProvider\ItemDataProviderInterface;
+use ApiPlatform\Core\DataProvider\RestrictedDataProviderInterface;
+use App\Entity\ImageStock;
+use Exception;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class ImageStockDataProvider implements RestrictedDataProviderInterface, CollectionDataProviderInterface, ItemDataProviderInterface
+{
+    const RESOURCE_TYPE_USER = 'user';
+    const RESOURCE_TYPE_GENERAL = 'general';
+    const RESOURCE_TYPE_NAME = 'resource_type';
+
+    /**
+     * @param string $directoryPath
+     * @param RequestStack $requestStack
+     */
+    public function __construct(private string $directoryPath, private RequestStack $requestStack)
+    {
+    }
+
+    /**
+     * @param string $resourceClass
+     * @param string|null $operationName
+     * @param array<mixed> $context
+     * @return bool
+     */
+    public function supports(string $resourceClass, string $operationName = null, array $context = []): bool
+    {
+        return $resourceClass == ImageStock::class;
+    }
+
+    /**
+     * @param string $resourceClass
+     * @param string|null $operationName
+     * @return array<ImageStock>
+     * @throws Exception
+     */
+    public function getCollection(string $resourceClass, string $operationName = null): array
+    {
+        if ($this->requestStack->getCurrentRequest() === null) {
+            throw new Exception('Request is null');
+        }
+        $resourceType = $this->requestStack->getCurrentRequest()->query->get(self::RESOURCE_TYPE_NAME);
+        if ($resourceType !== self::RESOURCE_TYPE_USER) {
+            $resourceType = self::RESOURCE_TYPE_GENERAL;
+        }
+        $path = $this->directoryPath . '/' . $resourceType;
+
+        $ls = scandir($path);
+        if ($ls === false) {
+            throw new Exception();
+        }
+        //Easy way to get rid of the dots that scandir() picks up in Linux environments:
+        $scannedDirectory = array_diff($ls, ['..', '.']);
+        $imagesPath = [];
+        foreach ($scannedDirectory as $scan) {
+            $imgPath = '/' . $this->directoryPath . '/' . $resourceType . '/' . $scan;
+            $imagesPath[] = new ImageStock($imgPath, $resourceType);
+        }
+
+        return $imagesPath;
+    }
+
+    /**
+     * @param string $resourceClass
+     * @param array<mixed>|int|object|string $id
+     * @param string|null $operationName
+     * @param array<mixed> $context
+     * @return ImageStock
+     */
+    public function getItem(string $resourceClass, $id, string $operationName = null, array $context = []): ImageStock
+    {
+        return new ImageStock('/app', 'user');
+    }
+}
---
+++ src/Entity/ImageStock.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiProperty;
+use ApiPlatform\Core\Annotation\ApiResource;
+
+#[ApiResource(
+    collectionOperations: [
+        'get' => [
+            'method' => 'GET',
+            'openapi_context' => [
+                'parameters' => [
+                    [
+                        'name' => 'resource_type',
+                        'in' => 'query'
+                    ]
+                ]
+            ],
+        ],
+    ],
+    itemOperations: ['get'],
+    paginationEnabled: false
+)]
+class ImageStock
+{
+    #[ApiProperty(
+        description: 'Directory resource name',
+        openapiContext: [
+            'Available resources' => 'User'
+        ],
+    )]
+    private string $resource;
+
+    #[ApiProperty(
+        description: 'Resource type'
+    )]
+    private string $resourceType;
+
+    #[ApiProperty(
+        identifier: true
+    )]
+    private string $id;
+
+
+    /**
+     * @param string $resource
+     * @param string $resourceType
+     */
+    public function __construct(string $resource, string $resourceType )
+    {
+        $this->resource = $resource;
+        $this->resourceType = $resourceType;
+        $this->id = (string) uniqid();
+    }
+
+    /**
+     * @return string
+     */
+    public function getResource(): string
+    {
+        return $this->resource;
+    }
+
+    /**
+     * @return string
+     */
+    public function getResourceType(): string
+    {
+        return $this->resourceType;
+    }
+
+    /**
+     * @return string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+}
